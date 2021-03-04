34afea6bee7f0fcbd2ea86337a2fc0eb636c2817
iameunbin <eunbin_dev@naver.com>
2021-03-04 16:03:31 +0900

    hello c++

---
+++ 210304/210304/test1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Hello C++" << std::endl;
+	std::cout << "열심히 공부할게요";
+
+	return 0;
+}
---
+++ 210304/210304/test2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+	std::cout << "다른 파일로 작성" << std::endl;
+	std::cout << "결과 확인!!";
+
+	return 0;
+}
